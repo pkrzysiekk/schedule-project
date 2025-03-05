f935fbce6298e5882267318bc04fb9df47eefa13
pkrzysiekk <pkrzysiek24@gmail.com>
2025-03-05 08:45:00 +0100

    Add main structure of the app

---
+++ App/App/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // instantiate a driver instance to control
+        // Chrome in headless mode
+        var chromeOptions = new ChromeOptions();
+        chromeOptions.AddArguments("--headless=new"); // comment out for testing
+        var driver = new ChromeDriver(chromeOptions);
+        driver.Navigate().GoToUrl("https://plany.ubb.edu.pl/plan.php?type=2&id=12626&winW=1341&winH=946&loadBG=000000");
+        var html = driver.PageSource;
+        
+
+
+        // scraping logic...
+
+        // close the browser and release its resources
+        driver.Quit();
+    }
+}
