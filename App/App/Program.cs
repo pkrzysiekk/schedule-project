a5d4375b18bdbe8424fc9ec9974851773c66d066
pkrzysiekk <pkrzysiek24@gmail.com>
2025-03-05 09:24:40 +0100

    Add functonality to search for tutors schedules

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -11,13 +11,35 @@ class Program
         chromeOptions.AddArguments("--headless=new"); // comment out for testing
         var driver = new ChromeDriver(chromeOptions);
         driver.Navigate().GoToUrl("https://plany.ubb.edu.pl/plan.php?type=2&id=12626&winW=1341&winH=946&loadBG=000000");
-        var html = driver.PageSource;
+        var productElements = driver.FindElements(By.CssSelector(".coursediv"));
+
+        foreach (var element in productElements)
+        {
+            var a = element.FindElements(By.CssSelector("a"));
+            var linkList=new List<string>();
+            foreach(var link in a)
+            {
+               var href=link.GetAttribute("href");
+               linkList.Add(href);
+
+            }
+            var tutors = linkList.Where(x => x.Contains("type=10"));
+            Console.WriteLine("Tutors:");
+            foreach(var tutor in tutors)
+            {
+                Console.WriteLine(tutor.ToString());
+            }
+
+        }
         
+
+
 
 
         // scraping logic...
 
         // close the browser and release its resources
         driver.Quit();
+      
     }
 }
