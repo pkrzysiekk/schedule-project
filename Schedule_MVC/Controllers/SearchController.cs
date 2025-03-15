afb7db8cd9a88040844bb4d8d71ae23e0a57925d
Paweł <pawelgawron2002@gmail.com>
2025-03-15 21:29:19 +0100

    Add action methods

--- Schedule_MVC/Controllers/SearchController.cs
+++ Schedule_MVC/Controllers/SearchController.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Schedule_MVC.Models;
 
 namespace Schedule_MVC.Controllers;
 
 public class SearchController : Controller
 {
+    private readonly AppDbContext _context;
+    public SearchController(AppDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var allTutors = _context.ScrapedData.GroupBy(x => x.CourseFullName)
+                                            .Select(r => r.First())
+                                            .ToList();
+        return View(allTutors);
+    }
+    public IActionResult FilteredSearch(int id)
+    {
+        if(id < 0)
+        {
+            RedirectToAction(nameof(Index));
+        }
+        var course = _context.ScrapedData.FirstOrDefault(x => x.ID == id);
+        if (course != null)
+        {
+            return View(course);
+        }
+        else { return NotFound(); }
     }
 }
