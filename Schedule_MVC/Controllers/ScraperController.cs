478e53886bdb96c3ed2b01ad4fdd0dcbc44d5bc5
Paweł <pawelgawron2002@gmail.com>
2025-03-15 20:48:01 +0100

    Update code, add async/await, return data from database where scrapped data is saved

--- Schedule_MVC/Controllers/ScraperController.cs
+++ Schedule_MVC/Controllers/ScraperController.cs
@@ -1,27 +1,28 @@
 ﻿using App.Models;
 using App.WebScrapers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Schedule_MVC.Models;
 
 namespace Schedule_MVC.Controllers;
 
 public class ScraperController : Controller
 {
+    private readonly AppDbContext _context;
     private string _json;
     private List<string> _links;
-
-    public ScraperController()
+    public ScraperController(AppDbContext context)
     {
+        _context = context;
         _json = System.IO.File.ReadAllText("links.json");
         _links = JsonConvert.DeserializeObject<List<string>>(_json);
     }
-
+    
     public async Task<IActionResult> Index()
     {
-        App.Controllers.ScraperController sc = new App.Controllers.ScraperController();
-        List<Tutor> allTutors = await sc.Scrape();
-
+        var tutors = await _context.ScrapedData.ToListAsync();
         // Przekazujemy dane do widoku
-        return View(allTutors);
+        return View(tutors);
     }
 }
