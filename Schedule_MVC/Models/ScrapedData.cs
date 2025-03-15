b0f891ee41c186d724f649ff98531028331442c3
Paweł <pawelgawron2002@gmail.com>
2025-03-15 18:45:01 +0100

    Add model of main table

---
+++ Schedule_MVC/Models/ScrapedData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule_MVC.Models;
+
+public class ScrapedData
+{
+    [Key]
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string CourseFullName { get; set; }
+    public string CourseType { get; set; }
+    public string IsLead { get; set; }
+}
