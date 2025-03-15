4b40a2f432a54d1fc69077b54d59e1b7a6a048ac
Paweł <pawelgawron2002@gmail.com>
2025-03-15 18:24:03 +0100

    Add Index action method

---
+++ Schedule_MVC/Controllers/ScraperController.cs
@@ -0,0 +1,27 @@
+﻿using App.Models;
+using App.WebScrapers;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Schedule_MVC.Controllers;
+
+public class ScraperController : Controller
+{
+    private string _json;
+    private List<string> _links;
+
+    public ScraperController()
+    {
+        _json = System.IO.File.ReadAllText("links.json");
+        _links = JsonConvert.DeserializeObject<List<string>>(_json);
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        App.Controllers.ScraperController sc = new App.Controllers.ScraperController();
+        List<Tutor> allTutors = await sc.Scrape();
+
+        // Przekazujemy dane do widoku
+        return View(allTutors);
+    }
+}
