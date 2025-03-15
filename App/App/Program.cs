8b3c8678824a448294dba443ec21ef1468c6ce76
Paweł <pawelgawron2002@gmail.com>
2025-03-15 18:19:08 +0100

    Update Program.cs in App project

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -5,7 +5,6 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        Stopwatch sw = new Stopwatch();
         ScraperController scraperController = new ScraperController();
 
         var formatedTutors = await scraperController.Scrape();
@@ -19,20 +18,18 @@ internal class Program
             Console.WriteLine(tutor.IsLead ? "Lead Tutor" : "Not lead");
             Console.WriteLine("*********************");
         }
-        Console.WriteLine("Enter subject name: ");
-        Console.WriteLine($"Time taken: {sw.ElapsedMilliseconds / 1000} s");
-        Console.WriteLine("Enter subject name: ");
+        //Console.WriteLine("Enter subject name: ");
 
-        string name = Console.ReadLine();
-        var tutorToFind = formatedTutors.Find(x => x.Course.courseShortName == name && x.IsLead);
+        //string name = Console.ReadLine();
+        //var tutorToFind = formatedTutors.Find(x => x.Course.courseShortName == name && x.IsLead);
 
-        if (tutorToFind != null)
-        {
-            Console.WriteLine($"Tutor found: {tutorToFind.Name}");
-        }
-        else
-        {
-            Console.WriteLine("Tutor not found");
-        }
+        //if (tutorToFind != null)
+        //{
+        //    Console.WriteLine($"Tutor found: {tutorToFind.Name}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Tutor not found");
+        //}
     }
 }
