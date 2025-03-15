2c1713b8e28733e5d799f6ed3424038159585182
Paweł <pawelgawron2002@gmail.com>
2025-03-15 20:55:16 +0100

    Add a new controller for search functionality

---
+++ Schedule_MVC/Controllers/SearchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Schedule_MVC.Controllers;
+
+public class SearchController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
