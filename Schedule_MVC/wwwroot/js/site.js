53e30a99454df6d5ba996445eeba99b0fe4f5133
Paweł <pawelgawron2002@gmail.com>
2025-03-16 14:38:42 +0100

    Add JS script for dark/light mode toggle button
    -Implement functionality to toggle between dark and light mode using a button. (positioned in the top right corner using Bootstrap class)
    -Add persistent theme state using localStorage to retain the selected mode. (page reloads)

--- Schedule_MVC/wwwroot/js/site.js
+++ Schedule_MVC/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('theme');
+    const body = document.body;
 
-// Write your JavaScript code.
+    //Sprawdzamy, czy uzytkownik ma zapisany preferowany tryb w przeglądarce
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-mode');
+        toggleButton.textContent = 'Switch to light mode';
+    }
+    else {
+        toggleButton.textContent = 'Switch to dark mode';
+    }
+
+    toggleButton.addEventListener('click', function () {
+        if (body.classList.contains('dark-mode')) {
+            body.classList.remove('dark-mode');
+            toggleButton.textContent = 'Switch to dark mode';
+            localStorage.setItem('theme', 'light');
+        } else {
+            body.classList.add('dark-mode');
+            toggleButton.textContent = 'Switch to light mode';
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+});
