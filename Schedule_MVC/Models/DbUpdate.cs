4c4ec4622d7c0bf014ba8cdf13dd519db84fa386
Paweł <pawelgawron2002@gmail.com>
2025-04-01 14:39:12 +0200

    Add a new class for Db content update

---
+++ Schedule_MVC/Models/DbUpdate.cs
@@ -0,0 +1,28 @@
+﻿using App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Schedule_MVC.Models;
+
+public class DbUpdate
+{
+    private readonly AppDbContext _context;
+    public DbUpdate(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task ClearDb()
+    {
+        try
+        {
+            var records = await _context.ScrapedData.ToListAsync();
+
+            _context.ScrapedData.RemoveRange(records);
+
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+        }
+    }
+}
